920434f25d07579eaaf7a0ed1723788b16a087bf
Abu <iliyanananan@gmail.com>
2022-05-31 14:11:56 +0300

    Simple problem

---
+++ JavaAdvanced - may 2022/src/_3_SetsAndMaps/ex_2_SetsOfElements2.java
@@ -0,0 +1,37 @@
+package _3_SetsAndMaps;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class ex_2_SetsOfElements2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] input = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int firstSetSize = input[0];
+        int secondSetSize = input[1];
+
+        Set<Integer> firstSet = new LinkedHashSet<>();
+        Set<Integer> secondSet = new LinkedHashSet<>();
+
+        for (int i = 0; i < firstSetSize; i++) {
+            int currentNumber = Integer.parseInt(scanner.nextLine());
+            firstSet.add(currentNumber);
+        }
+        for (int i = 0; i < secondSetSize; i++) {
+            int currentNumber = Integer.parseInt(scanner.nextLine());
+            secondSet.add(currentNumber);
+        }
+        for (Integer element : firstSet) {
+            if (secondSet.contains(element)) {
+                System.out.print(element + " ");
+            }
+        }
+
+    }
+}
