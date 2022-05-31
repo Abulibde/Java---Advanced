dc0ce15672a986f01e9617e8a47003c57506d1d7
Abu <iliyanananan@gmail.com>
2022-05-31 14:26:29 +0300

    problem with TreeSet

--- JavaAdvanced - may 2022/src/_3_SetsAndMaps/ex_3_PeriodicTable.java
+++ JavaAdvanced - may 2022/src/_3_SetsAndMaps/ex_3_PeriodicTable.java
@@ -1,5 +1,19 @@
 package _3_SetsAndMaps;
 
+/*
+You are given an n number of chemical compounds. You need to keep track of all
+chemical elements used in the compounds and at the end print all unique ones in ascending order:
+
+Examples
+Input	        Output
+4               Ce Ee Mo O
+Ce O
+Mo O Ce
+Ee
+Mo
+
+ */
+
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Set;
