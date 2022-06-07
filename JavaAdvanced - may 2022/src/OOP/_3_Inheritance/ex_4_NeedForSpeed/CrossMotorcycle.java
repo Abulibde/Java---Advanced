d908926a378a3fb9069bf2059ea0d1ceb0a7c260
Abu <iliyanananan@gmail.com>
2022-06-07 18:37:37 +0300

    upload new file

---
+++ JavaAdvanced - may 2022/src/OOP/_3_Inheritance/ex_4_NeedForSpeed/CrossMotorcycle.java
@@ -0,0 +1,8 @@
+package OOP._3_Inheritance.ex_4_NeedForSpeed;
+
+public class CrossMotorcycle extends Motorcycle {
+
+    public CrossMotorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
---
+++ JavaAdvanced - may 2022/src/OOP/_3_Inheritance/ex_4_NeedForSpeed/Motorcycle.java
@@ -0,0 +1,8 @@
+package OOP._3_Inheritance.ex_4_NeedForSpeed;
+
+public class Motorcycle extends Vehicle {
+
+    public Motorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
