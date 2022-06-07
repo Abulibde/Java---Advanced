d91a6ec2b3a0f607795bfea4244c9fc5ca01ca05
Abu <iliyanananan@gmail.com>
2022-06-07 18:38:02 +0300

    upload new file

---
+++ JavaAdvanced - may 2022/src/OOP/_3_Inheritance/ex_4_NeedForSpeed/Vehicle.java
@@ -0,0 +1,48 @@
+package OOP._3_Inheritance.ex_4_NeedForSpeed;
+
+public class Vehicle {
+
+    final static double DEFAULT_FUEL_CONSUMPTION = 1.25;
+    private double fuelConsumption;
+    private double fuel;
+    private int horsePower;
+
+    public Vehicle(double fuel, int horsePower) {
+        this.fuel = fuel;
+        this.horsePower = horsePower;
+        this.fuelConsumption = DEFAULT_FUEL_CONSUMPTION;
+    }
+
+    public void drive(double kilometers) {
+
+        double fuelNeeded = kilometers * fuelConsumption;
+
+        if (fuel > fuelNeeded) {
+            fuel -= fuelNeeded;
+        }
+    }
+
+    public double getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public void setFuelConsumption(double fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public double getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(double fuel) {
+        this.fuel = fuel;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+}
