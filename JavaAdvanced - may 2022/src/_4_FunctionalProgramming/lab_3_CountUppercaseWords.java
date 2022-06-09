a22965fc9f94ad6279eb93347ca0d14ebe8595e2
Abu <iliyanananan@gmail.com>
2022-06-09 18:33:43 +0300

    Predicate

---
+++ JavaAdvanced - may 2022/src/_4_FunctionalProgramming/lab_3_CountUppercaseWords.java
@@ -0,0 +1,40 @@
+package _4_FunctionalProgramming;
+
+/*
+3.	Count Uppercase Words
+Write a program that reads one line of text from the console.
+Print the count of words that start with an Uppercase letter,
+after that print all these words in the same order, as you found them in the text.
+Use a Predicate<String>.
+
+
+Examples
+Input	                                                                        Output
+The following example shows how to use Predicate	                            2
+                                                                                The
+                                                                                Predicate
+ */
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class lab_3_CountUppercaseWords {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        Predicate<String> upperCase = w -> Character.isUpperCase(w.charAt(0));
+
+
+        List<String> uppercaseWords = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .filter(upperCase)
+                .collect(Collectors.toList());
+
+        System.out.println(uppercaseWords.size());
+
+        System.out.println(String.join(System.lineSeparator(), uppercaseWords));
+    }
+}
