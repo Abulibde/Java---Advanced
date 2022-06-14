5085ddece3f7e8431483f613f74f7993b0088d8b
Abu <iliyanananan@gmail.com>
2022-06-14 23:19:06 +0300

    new exercise

--- JavaAdvanced - may 2022/src/Advanced/_2_MultidimensionalArrays/ex_5_MatrixShuffling.java
+++ JavaAdvanced - may 2022/src/Advanced/_2_MultidimensionalArrays/ex_5_MatrixShuffling.java
@@ -1,4 +1,4 @@
-package Advanced._1_StacksAndQueues._2_MultidimensionalArrays;
+package Advanced._2_MultidimensionalArrays;
 
 /*
 Write a program, which reads a string matrix from the console and performs certain operations
