e029b436e7deeb0a7961b5af7a069903ddc7eca0
Abu <iliyanananan@gmail.com>
2022-06-24 18:06:20 +0300

    Grooming salon

---
+++ JavaAdvanced - may 2022/src/ExamPreparation/June2021/groomingSalon/GroomingSalon.java
@@ -0,0 +1,48 @@
+package ExamPreparation.June2021.groomingSalon;
+
+import hotel.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroomingSalon {
+    private int capacity;
+    private List<Pet> data;
+
+    public GroomingSalon(int capacity) {
+        this.capacity = capacity;
+        data = new ArrayList<>();
+    }
+
+    public void add(Pet pet) {
+        if (capacity > data.size()) {
+            data.add(pet);
+        }
+    }
+
+    public boolean remove(String name){
+        return data.removeIf(pet->pet.getName().equals(name));
+    }
+
+    public Pet getPet(String name, String owner){
+        return data.stream().
+                filter(pet -> pet.getName().equals(name)
+                        && pet.getOwner().equals(owner))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public int getCount(){
+        return data.size();
+    }
+
+    public String getStatistics() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("The grooming salon has the following clients:%n"));
+
+        for (Pet pet : data) {
+            sb.append(String.format("%s%n",pet.toString()));
+        }
+        return sb.toString();
+    }
+}
