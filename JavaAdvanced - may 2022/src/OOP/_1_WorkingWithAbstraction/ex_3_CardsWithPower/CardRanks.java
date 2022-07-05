ebd7026c0b26fb21591eea191c9855b502873512
Abu <iliyanananan@gmail.com>
2022-07-05 15:48:45 +0300

    upload new file

---
+++ JavaAdvanced - may 2022/src/OOP/_1_WorkingWithAbstraction/ex_3_CardsWithPower/CardRanks.java
@@ -0,0 +1,31 @@
+package OOP._1_WorkingWithAbstraction.ex_3_CardsWithPower;
+
+public enum CardRanks {
+    ACE(14),
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    JACK(11),
+    QUEEN(12),
+    KING(13);
+
+    private int rankPower;
+
+    CardRanks(int rankPower) {
+        this.rankPower = rankPower;
+    }
+
+    public int getRankPower() {
+        return rankPower;
+    }
+
+    public void setRankPower(int rankPower) {
+        this.rankPower = rankPower;
+    }
+}
