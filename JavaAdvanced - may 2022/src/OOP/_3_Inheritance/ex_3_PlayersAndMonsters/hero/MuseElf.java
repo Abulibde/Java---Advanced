1e1a120a92b392fc7bea99738f89f8cc4fb03e24
Abu <iliyanananan@gmail.com>
2022-07-12 18:05:55 +0300

    upload new file

---
+++ JavaAdvanced - may 2022/src/OOP/_3_Inheritance/ex_3_PlayersAndMonsters/hero/MuseElf.java
@@ -0,0 +1,8 @@
+package OOP._3_Inheritance.ex_3_PlayersAndMonsters.hero;
+
+public class MuseElf extends Elf {
+
+    public MuseElf(String username, int level) {
+        super(username, level);
+    }
+}
