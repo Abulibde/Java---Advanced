93e0ba916dbdeed070384add11960ea6abbabdcd
Abu <iliyanananan@gmail.com>
2022-07-12 18:06:00 +0300

    upload new file

---
+++ JavaAdvanced - may 2022/src/OOP/_3_Inheritance/ex_3_PlayersAndMonsters/hero/SoulMaster.java
@@ -0,0 +1,7 @@
+package OOP._3_Inheritance.ex_3_PlayersAndMonsters.hero;
+
+public class SoulMaster extends DarkWizard {
+    public SoulMaster(String username, int level) {
+        super(username, level);
+    }
+}
