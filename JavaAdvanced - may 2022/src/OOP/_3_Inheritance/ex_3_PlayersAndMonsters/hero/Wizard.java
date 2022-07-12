21b2a2375a3b47d27a27be85bad1a724b88d4560
Abu <iliyanananan@gmail.com>
2022-07-12 18:06:06 +0300

    upload new file

---
+++ JavaAdvanced - may 2022/src/OOP/_3_Inheritance/ex_3_PlayersAndMonsters/hero/Wizard.java
@@ -0,0 +1,7 @@
+package OOP._3_Inheritance.ex_3_PlayersAndMonsters.hero;
+
+public class Wizard extends Hero {
+    public Wizard(String username, int level) {
+        super(username, level);
+    }
+}
