7695f08c43829aa71ec4722da55b5a27fde4a5b5
Abu <iliyanananan@gmail.com>
2022-07-12 17:47:13 +0300

    upload new file

---
+++ JavaAdvanced - may 2022/src/OOP/_3_Inheritance/ex_2_Zoo/zoo/Animal.java
@@ -0,0 +1,13 @@
+package OOP._3_Inheritance.ex_2_Zoo.zoo;
+
+public class Animal {
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
