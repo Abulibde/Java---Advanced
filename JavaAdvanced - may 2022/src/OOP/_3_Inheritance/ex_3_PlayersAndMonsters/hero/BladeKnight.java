5c2e26f096a049c88f2f13d2c4b359b59e3cc11c
Abu <iliyanananan@gmail.com>
2022-07-12 18:05:16 +0300

    upload new file

---
+++ JavaAdvanced - may 2022/src/OOP/_3_Inheritance/ex_3_PlayersAndMonsters/hero/BladeKnight.java
@@ -0,0 +1,7 @@
+package OOP._3_Inheritance.ex_3_PlayersAndMonsters.hero;
+
+public class BladeKnight extends DarkKnight {
+    public BladeKnight(String username, int level) {
+        super(username, level);
+    }
+}
