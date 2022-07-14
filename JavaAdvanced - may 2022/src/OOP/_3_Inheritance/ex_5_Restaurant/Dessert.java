5b32a350af31f531a748f7455ca47265c3029f27
Abu <iliyanananan@gmail.com>
2022-07-14 14:20:06 +0300

    upload new file

---
+++ JavaAdvanced - may 2022/src/OOP/_3_Inheritance/ex_5_Restaurant/Dessert.java
@@ -0,0 +1,17 @@
+package OOP._3_Inheritance.ex_5_Restaurant;
+
+import java.math.BigDecimal;
+
+public class Dessert extends Food {
+
+    private double calories;
+
+    public Dessert(String name, BigDecimal price, double grams, double calories) {
+        super(name, price, grams);
+        this.calories = calories;
+    }
+
+    public double getCalories() {
+        return calories;
+    }
+}
