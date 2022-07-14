3f5a0b0c818de946e2843517d15d1c30ac1d18e3
Abu <iliyanananan@gmail.com>
2022-07-14 14:19:38 +0300

    upload new file

---
+++ JavaAdvanced - may 2022/src/OOP/_3_Inheritance/ex_5_Restaurant/Product.java
@@ -0,0 +1,21 @@
+package OOP._3_Inheritance.ex_5_Restaurant;
+
+import java.math.BigDecimal;
+
+public class Product {
+    private String name;
+    private BigDecimal price;
+
+    public Product(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+}
