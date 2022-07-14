77e224c8c029c721fbc0ddecb8c7089ac7d9be15
Abu <iliyanananan@gmail.com>
2022-07-14 14:19:55 +0300

    upload new file

---
+++ JavaAdvanced - may 2022/src/OOP/_3_Inheritance/ex_5_Restaurant/HotBeverage.java
@@ -0,0 +1,10 @@
+package OOP._3_Inheritance.ex_5_Restaurant;
+
+import java.math.BigDecimal;
+
+public class HotBeverage extends Beverage {
+
+    public HotBeverage(String name, BigDecimal price, double milliliters) {
+        super(name, price, milliliters);
+    }
+}
