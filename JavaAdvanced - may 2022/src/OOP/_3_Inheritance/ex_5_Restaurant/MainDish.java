26adf615cf6219648426bc873dcc0fceb0db1d7c
Abu <iliyanananan@gmail.com>
2022-07-14 14:19:43 +0300

    upload new file

---
+++ JavaAdvanced - may 2022/src/OOP/_3_Inheritance/ex_5_Restaurant/MainDish.java
@@ -0,0 +1,10 @@
+package OOP._3_Inheritance.ex_5_Restaurant;
+
+import java.math.BigDecimal;
+
+public class MainDish extends Food {
+
+    public MainDish(String name, BigDecimal price, double grams) {
+        super(name, price, grams);
+    }
+}
