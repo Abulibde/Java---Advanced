77ce8a87eae4f5ccf512f9af73c2e172d3b384ee
Abu <iliyanananan@gmail.com>
2022-07-14 14:18:54 +0300

    upload new file

---
+++ JavaAdvanced - may 2022/src/OOP/_3_Inheritance/ex_5_Restaurant/Beverage.java
@@ -0,0 +1,17 @@
+package OOP._3_Inheritance.ex_5_Restaurant;
+
+import java.math.BigDecimal;
+
+public class Beverage extends Product {
+
+    private double milliliters;
+
+    public Beverage(String name, BigDecimal price, double milliliters) {
+        super(name, price);
+        this.milliliters = milliliters;
+    }
+
+    public double getMilliliters() {
+        return milliliters;
+    }
+}
