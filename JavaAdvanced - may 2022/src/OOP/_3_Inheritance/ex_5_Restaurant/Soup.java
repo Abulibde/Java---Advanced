d1067222560c0323630aff1ebf63750e69c21fcd
Abu <iliyanananan@gmail.com>
2022-07-14 14:19:23 +0300

    upload new file

---
+++ JavaAdvanced - may 2022/src/OOP/_3_Inheritance/ex_5_Restaurant/Soup.java
@@ -0,0 +1,10 @@
+package OOP._3_Inheritance.ex_5_Restaurant;
+
+import java.math.BigDecimal;
+
+public class Soup extends Starter {
+
+    public Soup(String name, BigDecimal price, double grams) {
+        super(name, price, grams);
+    }
+}
