4f643d6fdb45a37b16edab349ca520423cc66579
Abu <iliyanananan@gmail.com>
2022-07-14 14:19:17 +0300

    upload new file

---
+++ JavaAdvanced - may 2022/src/OOP/_3_Inheritance/ex_5_Restaurant/Tea.java
@@ -0,0 +1,10 @@
+package OOP._3_Inheritance.ex_5_Restaurant;
+
+import java.math.BigDecimal;
+
+public class Tea extends HotBeverage {
+
+    public Tea(String name, BigDecimal price, double milliliters) {
+        super(name, price, milliliters);
+    }
+}
